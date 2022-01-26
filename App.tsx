de1c2a132a034332df524437936f8bbc998b83ad
Radium Rocket <radiumrocket31@Radium-31.local>
2022-01-26 12:38:17 -0300

    Create clientType, fetch clients and show client name in FlatList component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
+  View,
+  FlatList
 } from 'react-native';
+
+  type clientType = {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: {
+      street: string,
+      suite: string,
+      city: string,
+      zipcode: string,
+      geo: {
+        lat: number,
+        lng: number
+      }
+    },
+    phone: string,
+    website: string,
+    company: {
+      name: string,
+      catchPhrase: string,
+      bs: string
+    }
+  }
 
 const App = () => {
 
+  const [clients, setClients] = useState<clientType[]>([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( async (response) => await response.json())
+    .then((response) => {
+      setClients(response)
+    })
+    .catch((error) => { error});
+  }, [])
+
   return (
     <SafeAreaView>
+      <View>
       <Text style={styles.highlight}>React</Text>
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={clients}
+        renderItem={({item}) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+      </View>
     </SafeAreaView>
   );
 };
@@ -18,6 +64,9 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  item: {
+
+  }
 });
 
 export default App;
