42007594412ddfeb887f477e300292cc0440969a
Radium Rocket <radiumrocket31@Radium-31.local>
2022-02-05 19:43:24 -0300

    Implement image picker for camera and gallery

--- src/Components/User/index.tsx
+++ src/Components/User/index.tsx
@@ -1,39 +1,96 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {launchImageLibrary} from 'react-native-image-picker';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import {launchImageLibrary, launchCamera} from 'react-native-image-picker';
+
+interface Options {
+  title?: string;
+  storageOptions?: {
+    skipBackup?: boolean;
+    path?: string;
+    mediaType?: string;
+  };
+  includeBase64?: boolean;
+}
 
 const User = () => {
+  const [image, setImage] = useState(
+    '/Users/radiumrocket31/projects/MyApp/react-native-with-typescript/assets/defaultUserImage.png',
+  );
+
+  const openGallery = () => {
+    const options: Options = {
+      title: 'Select image from',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+        mediaType: 'photo',
+      },
+      includeBase64: true,
+    };
+    launchImageLibrary(options, response => {
+      if (response.errorCode) {
+        console.log(response.errorMessage);
+      } else if (response.didCancel) {
+        console.log('User cancel');
+      } else {
+        setImage(response.assets[0].uri);
+      }
+    });
+  };
+
+  const openCamera = () => {
+    const options: Options = {
+      title: 'Select an image',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+        mediaType: 'photo',
+      },
+      includeBase64: true,
+    };
+    launchCamera(options, response => {
+      if (response.errorCode) {
+        console.log(response.errorMessage);
+      } else if (response.didCancel) {
+        console.log('User cancel');
+      } else {
+        setImage(response.assets[0].uri);
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>User</Text>
-      <Button
-        title="Gallery"
-        onPress={async () => {
-          const options = {
-            title: 'Gallery',
-            customButtons: [{name: 'fb', title: 'Facebook'}],
-            cancelButton: 'Cancel',
-            takePhotoButtonTitle: 'Take a picture',
-            chooseFromLibraryButtonTitle: 'Open Gallery',
-            noData: true,
-          };
-          await launchImageLibrary(options, (res: any) => {
-            console.log({res});
-          });
-          // imagePicker.launchCamera();
-          // imagePicker.launchImageLibrary();
-        }}
-      />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Profile</Text>
+      </View>
+      <Image style={styles.image} source={{uri: image}} />
+      <View>
+        <Button title="Take a picture" onPress={openCamera} />
+        <Button title="Open Gallery" onPress={openGallery} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    height: '100%',
-    backgroundColor: '#FFFFFF',
+  },
+  titleContainer: {
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    color: '#19456B',
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 200,
+    borderColor: 'red',
   },
 });
 
